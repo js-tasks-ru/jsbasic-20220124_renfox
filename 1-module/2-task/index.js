1e3347d586a2ff720ca798a04714eeb01975998c
Alexey Loginov <aleksio2011@gmail.com>
2022-01-31 16:21:49 +0300

    task 2

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -10,7 +10,11 @@ function print(text) {
  * чтобы функция sayHello работала корректно
  */
 function isValid(name) {
-  // ваш код...
+  if ( !!name && name.length >= 4 &&  !name.includes(' ')) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function sayHello() {
