17326296d8492169ff8202c025ce7eab434b52a0
Alexey Loginov <aleksio2011@gmail.com>
2022-02-09 12:03:49 +0300

    task 2 mod 4

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,3 +1,27 @@
 function makeDiagonalRed(table) {
-  // ваш код...
-}
+  
+    let rowsLength = table.rows.length;
+    let rows = table.rows;
+  
+    for (let i = 0; i < rowsLength; i++) {
+      let row = rows[i];
+      row.cells[i].style.backgroundColor = 'red';
+    }
+  }
+
+
+
+// Моё решение.
+/*
+function makeDiagonalRed(table) {
+  let trows = document.querySelector("table");
+
+  for (let i = 0; i < trows.rows.length; i++) {
+    let row = trows.rows[i];
+    row.cells[i].style.backgroundColor = 'red';
+  }
+ 
+  }
+  */
+// Не понимаю почему не проходят тесты, по сути, всё тоже самое что 
+// и в решении преподавателя, но ни один тест не прошел
