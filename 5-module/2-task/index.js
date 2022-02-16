46a631bb8c4dc6d4cc87d999a8369f407308dfbc
Alexey Loginov <aleksio2011@gmail.com>
2022-02-16 11:37:24 +0300

    mod 5 task 1 2

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,5 +1,8 @@
 function toggleText() {
+<<<<<<< HEAD
   // ваш код...
+=======
+>>>>>>> 3e67c8bba6370da056995e6fdbb0e71c8c4a3ddd
   let toggleButton = document.querySelector('.toggle-text-button');
   let textElement = document.getElementById('text');
 
