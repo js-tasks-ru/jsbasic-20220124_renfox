7043586233f52d309509e8bc84de938490e3a2fb
Alexey Loginov <aleksio2011@gmail.com>
2022-03-01 15:55:02 +0300

    mod 7 task 7-2

--- 7-module/2-task/index.js
+++ 7-module/2-task/index.js
@@ -3,39 +3,40 @@ import createElement from '../../assets/lib/create-element.js';
 export default class Modal {
   constructor() {
     
-   
-    this.open();
+   this.render();
+    this.close();
   }
-
+  render() {
+    this.elem = createElement(`
+      <div class="modal">
+        <div class="modal__overlay"></div>
+        <div class="modal__inner">
+          <div class="modal__header">
+            <button type="button" class="modal__close">
+              <img src="/assets/images/icons/cross-icon.svg" alt="close-icon" />
+            </button>
+            <h3 class="modal__title"></h3>
+          </div>
+          <div class="modal__body"></div>
+        </div>
+      </div>
+    `);
+}
   
     open() {
-      document.body.classList.toggle('is-modal-open');
-      this.modal = document.querySelector('.container');
-      this.modal.innerHTML = `
-        <div class="modal">
-          <!--Прозрачная подложка перекрывающая интерфейс-->
-          <div class="modal__overlay"></div>
-  
-          <div class="modal__inner">
-            <div class="modal__header">
-              <!--Кнопка закрытия модального окна-->
-              <button type="button" class="modal__close">
-                <img src="../../assets/images/icons/cross-icon.svg" alt="close-icon" />
-              </button>
-  
-              <h3 class="modal__title">
-                Вот сюда нужно добавлять заголовок
-              </h3>
-            </div>
-  
-            <div class="modal__body">
-              A сюда нужно дsобавлять содержимое тела модального окна
-            </div>
-          </div>
-  
-      </div>
-  `;
-      
+      document.body.classList.add('is-modal-open');
+      document.body.append(this.elem);
     };
-  
+    setTitle(title) {
+      this.elem.querySelector('.modal__title').textContent = title;
+  }
+  setBody(node) {
+    this.elem.querySelector('.modal__body').innerHTML = node.outerHTML;
+  }
+  close() {
+    this.elem.querySelector('.modal__close').addEventListener('click', function() {
+      document.body.classList.remove('is-modal-open');
+      document.querySelector('.modal').remove();
+    });
+  }
 }
